fc960c1cbabe8ccdde942203cefc5746bfd752cf
Gino Canessa <gino.canessa@microsoft.com>
2022-05-27 12:53:54 -0500

    Build from updated source

--- core/r4/dist/fhir/FhirPositiveInt.d.ts
+++ core/r4/dist/fhir/FhirPositiveInt.d.ts
@@ -37,5 +37,29 @@ export declare class FhirPositiveInt extends fhir.FhirPrimitive {
      * Function to perform basic model validation (e.g., check if required elements are present).
      */
     doModelValidation(): fhir.FtsIssue[];
+    /**
+     * Returns a string representation of an object.
+     * @param radix Specifies a radix for converting numeric values to strings. This value is only used for numbers.
+     */
+    toString(radix?: number): string;
+    /**
+     * Returns a string representing a number in fixed-point notation.
+     * @param fractionDigits Number of digits after the decimal point. Must be in the range 0 - 20, inclusive.
+     */
+    toFixed(fractionDigits?: number): string;
+    /**
+     * Returns a string containing a number represented in exponential notation.
+     * @param fractionDigits Number of digits after the decimal point. Must be in the range 0 - 20, inclusive.
+     */
+    toExponential(fractionDigits?: number): string;
+    /**
+     * Returns a string containing a number represented either in exponential or fixed-point notation with a specified number of digits.
+     * @param precision Number of significant digits. Must be in the range 1 - 21, inclusive.
+     */
+    toPrecision(precision?: number): string;
+    /**
+     * Returns the primitive value of the specified object.
+     */
+    valueOf(): number;
 }
 //# sourceMappingURL=FhirPositiveInt.d.ts.map
